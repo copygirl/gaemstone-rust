4ca86c26099c0ce438549cb7b0faeee7f72eb53b
copygirl <copygirl@mcft.net>
2020-05-26 18:42:46 +0200

    Add PaletteStore unit tests, fix bugs
    - Fix `get_free_palette_index` considering default palette entry as free
    - Fix first palette entry being added not reducing default `ref_count`
    - Fix bits growing copying the wrong part of the slice
    - Fix shrinking panicking incorrectly

--- src/util/palette_store.rs
+++ src/util/palette_store.rs
@@ -1,6 +1,8 @@
-use bitvec::{bitvec, slice::AsBits, vec::BitVec};
+use bitvec::prelude::*;
 
 const DEFAULT_CAPACITY: usize = 32;
+
+// TODO: Currently there's no public function to shrink the palette store.
 
 /// This data structure contains a set amount of virtual elements which can be read using `get()`
 /// and written using `set()` using a simple index bound by the size given to the palette store's
@@ -133,10 +135,11 @@ impl<T: Default + Copy + Eq> PaletteStore<T> {
   }
 
   pub unsafe fn set_unchecked(&mut self, index: usize, value: T) {
+    // Test if no palette entries are currently being used
+    // (such as when the palette store was just created).
     if self.used == 0 {
-      // If no palette entries are currently being used (such as when the palette
-      // store was just created), and the virtual element is being set to the default
-      // value (which would not change what is returned by `get()`), do nothing.
+      // If the virtual element is being set to the default value
+      // (which would not change what is returned by `get()`), do nothing.
       if value == Default::default() {
         return;
       }
@@ -166,15 +169,17 @@ impl<T: Default + Copy + Eq> PaletteStore<T> {
         return;
       }
 
-      // Need to re-borrow `entries`, else we can't `iter()` on it earlier.
-      let mut current = &mut self.entries[palette_index];
-      // If it just so happens that we freed up the old palette
-      // entry, we can replace it to refer to the new value.
-      if current.ref_count == 0 && palette_index > 0 {
-        current.value = value;
-        current.ref_count = 1;
-        self.used += 1;
-        return;
+      if palette_index > 0 {
+        // Need to re-borrow `entries`, else we can't `iter()` on it earlier.
+        let mut current = &mut self.entries[palette_index];
+        // If it just so happens that we freed up the old palette
+        // entry, we can replace it to refer to the new value.
+        if current.ref_count == 0 {
+          current.value = value;
+          current.ref_count = 1;
+          self.used += 1;
+          return;
+        }
       }
     }
 
@@ -192,11 +197,21 @@ impl<T: Default + Copy + Eq> PaletteStore<T> {
   fn get_free_palette_index(&mut self) -> usize {
     // Test to see if there should be a free palette entry and, if so, return its index.
     if self.free_entries() > 0 {
-      self.entries.iter().position(|e| e.ref_count == 0).unwrap()
+      self
+        .entries
+        .iter()
+        .skip(1) // Palette entry 0 is default - it is never considered "free".
+        .position(|entry| entry.ref_count == 0)
+        .unwrap()
+        + 1 // Since we skip entry 0 we need to add 1.
     } else {
       // If `entries` is empty, initialize capacity to DEFAULT_CAPACITY.
       if self.entries.is_empty() {
         self.reserve(DEFAULT_CAPACITY);
+        // NOTE: We're just going to assume that the palette index is actually being used, and since
+        //       this is the first palette entry being added, the `ref_count` for the default entry
+        //       will not be decremented in `set_unchecked`, so we do that here:
+        self.entries[0].ref_count -= 1;
         // Index 0 is already in use by the default value, so return 1 instead.
         1
       // Otherwise, reserve at least one additional element. This will cause the capacity to double,
@@ -230,13 +245,12 @@ impl<T: Default + Copy + Eq> PaletteStore<T> {
     } else if num_bits > self.bits_per_entry {
       // Build new bit vector, going through each element slice and copying it from the old data.
       let mut new_bits = bitvec![0; self.size * num_bits];
-      let skip_bits = num_bits - self.bits_per_entry;
       for (old, new) in self
         .bits
         .chunks(self.bits_per_entry)
         .zip(new_bits.chunks_mut(num_bits))
       {
-        new[skip_bits..].copy_from_slice(old);
+        new[..self.bits_per_entry].copy_from_slice(old);
       }
       self.bits = new_bits;
 
@@ -245,7 +259,7 @@ impl<T: Default + Copy + Eq> PaletteStore<T> {
     // If `bits_per_entry` shrinks, reorganize palette entries and recreate underlying bit vector.
     } else {
       assert!(
-        self.used_entries() > (1 << num_bits),
+        self.used_entries() < (1 << num_bits),
         "Attempted to shrink, but can't fit currently used entries"
       );
 
@@ -306,4 +320,124 @@ fn integer_log2(mut i: usize) -> usize {
     power += 1;
   }
   power
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn palette_entry_checks() {
+    let mut storage = PaletteStore::<u8>::new(16);
+
+    // When initializing with `new`, no additional data is allocated.
+    assert!(storage.entries.is_empty());
+    assert_eq!(storage.free_entries(), 0);
+    assert_eq!(storage.used_entries(), 0);
+
+    // Setting an element to the default value does not allocate data, either.
+    storage.set(0, 0u8).unwrap();
+    assert_eq!(storage.get(0).unwrap(), 0u8);
+    assert!(storage.entries.is_empty());
+
+    storage.set(0, 1u8).unwrap();
+    assert_eq!(storage.get(0).unwrap(), 1u8);
+    assert_eq!(storage.get(1).unwrap(), 0u8); // Sanity check: Element 1 should still be value 0u8.
+    assert_eq!(storage.entries.len(), DEFAULT_CAPACITY);
+    assert_eq!(storage.entries[0].value, 0u8);
+    assert_eq!(storage.entries[0].ref_count, 15);
+    assert_eq!(storage.entries[1].value, 1u8);
+    assert_eq!(storage.entries[1].ref_count, 1);
+
+    // Fill storage so its element layout will be:
+    // 1 2 3 4  5 6 7 8  9 10 11 12  13 14 15 16
+    for i in 1..16 {
+      storage.set(i, i as u8 + 1).unwrap();
+    }
+
+    // The default entry will not be re-used even if its `ref_count` goes to zero.
+    assert_eq!(storage.entries[0].value, 0u8);
+    assert_eq!(storage.entries[0].ref_count, 0);
+
+    for i in 0..16 {
+      assert_eq!(storage.get(i).unwrap(), i as u8 + 1);
+      assert_eq!(storage.entries[i + 1].value, i as u8 + 1);
+      assert_eq!(storage.entries[i + 1].ref_count, 1);
+    }
+
+    // Fill storage so its element layout will be:
+    // 1 1 1 1  1 1 1 1  1 1 1 1  1 1 1 1
+    for i in 0..16 {
+      storage.set(i, 1u8).unwrap();
+    }
+
+    // Default entry should stay untouched.
+    assert_eq!(storage.entries[0].value, 0u8);
+    assert_eq!(storage.entries[0].ref_count, 0);
+
+    // Second entry should now take up the entire storage.
+    assert_eq!(storage.entries[1].value, 1u8);
+    assert_eq!(storage.entries[1].ref_count, 16);
+
+    // Third entry's value should have been set back to the default.
+    assert_eq!(storage.entries[2].value, 0u8);
+    assert_eq!(storage.entries[2].ref_count, 0);
+
+    // Reset storage so its element layout will be:
+    // 0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0
+    for i in 0..16 {
+      storage.set(i, 0u8).unwrap();
+    }
+
+    // Default entry should once again take up the entire storage.
+    assert_eq!(storage.entries[0].value, 0u8);
+    assert_eq!(storage.entries[0].ref_count, 16);
+
+    // Second entry's value should have been set back to the default.
+    assert_eq!(storage.entries[1].value, 0u8);
+    assert_eq!(storage.entries[1].ref_count, 0);
+  }
+
+  #[test]
+  fn bits_layout_in_memory() {
+    let mut storage = PaletteStore::<i32>::new_with_capacity(8, 8);
+    assert_eq!(
+      storage.bits, // Element:         7   6   5   4   3   2   1   0
+      BitVec::<Local>::from_element(0b000_000_000_000_000_000_000_000)[..3 * 8]
+    );
+
+    // Fill out all palette entries.
+    for i in 0..8 {
+      storage.set(i, i as i32 * 32).unwrap();
+    }
+    assert_eq!(
+      storage.bits, // Element:         7   6   5   4   3   2   1   0
+      BitVec::<Local>::from_element(0b111_110_101_100_011_010_001_000)[..3 * 8]
+    );
+
+    // Add yet another, new palette entry, causing palettes to resize.
+    storage.set(0, i32::MAX).unwrap();
+    assert_eq!(
+      storage.bits, // Element:          7    6    5    4    3    2    1    0
+      BitVec::<Local>::from_element(0b0111_0110_0101_0100_0011_0010_0001_1000)[..4 * 8]
+    );
+
+    // Unuse all palette entries but 3 (including the default).
+    for i in &[0usize, 1, 3, 4, 6, 7] {
+      storage.set(*i, 0).unwrap();
+    }
+    assert_eq!(
+      storage.bits, // Element:          7    6    5    4    3    2    1    0
+      BitVec::<Local>::from_element(0b0000_0000_0101_0000_0000_0010_0000_0000)[..4 * 8]
+    );
+
+    // Shrink palette to fit 4 entries (2 bits each).
+    storage.set_bits_per_entry(2);
+    // Previous palette entry 2 (0010) should now be 1 (01),
+    //                    and 5 (0101) should now be 2 (10).
+    assert_eq!(
+      storage.bits, // Element:        7  6  5  4  3  2  1  0
+      BitVec::<Local>::from_element(0b00_00_10_00_00_01_00_00)[..2 * 8]
+    );
+  }
 }
